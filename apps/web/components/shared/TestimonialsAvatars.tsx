7fc86af79521e07277c577d57a3f66a505ada9cf
ycfra <yayecaceres@gmail.com>
2024-04-02 12:40:38 +0100

    testimonials fixed

--- apps/web/components/shared/TestimonialsAvatars.tsx
+++ apps/web/components/shared/TestimonialsAvatars.tsx
@@ -29,37 +29,40 @@ const avatars: {
 
 const TestimonialsAvatars = ({ priority }: { priority?: boolean }) => {
   return (
-    <div className="relative z-[2] flex items-center justify-center gap-3 lg:flex-row lg:justify-start">
-      <div className="avatar-group justy-start -space-x-5">
-        {avatars.map((image, i) => (
-          <div
-            className="avatar relative inline-flex h-12 w-12 overflow-hidden rounded-full border-2 border-white"
-            key={i}>
-            <Image src={image.src} alt={image.alt} priority={priority} width={50} height={50} />
-          </div>
-        ))}
-      </div>
-
-      <div className="my-auto flex flex-col items-center justify-center gap-1 md:items-start">
-        <div className="relative inline-flex">
-          {[...Array(5)].map((_, i) => (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5 text-yellow-500"
+    <div className="relative z-[2] flex flex-col items-center justify-center gap-3 lg:flex-row lg:justify-start">
+      <div className="flex flex-col md:flex-row">
+        <div className="avatar-group justify-start -space-x-5">
+          {avatars.map((image, i) => (
+            <div
+              className="avatar relative inline-flex h-12 w-12 overflow-hidden rounded-full border-2 border-white"
               key={i}>
-              <path
-                fillRule="evenodd"
-                d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                clipRule="evenodd"
-              />
-            </svg>
+              <Image src={image.src} alt={image.alt} priority={priority} width={50} height={50} />
+            </div>
           ))}
         </div>
+      </div>
+      <div className="flex-1">
+        <div className="my-auto flex flex-col items-center justify-center gap-1 lg:items-start">
+          <div className="relative inline-flex">
+            {[...Array(5)].map((_, i) => (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5 text-yellow-500"
+                key={i}>
+                <path
+                  fillRule="evenodd"
+                  d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            ))}
+          </div>
 
-        <div className="text-base-content/80 text-base">
-          <span className="text-base-content font-semibold">1,567+</span> AI Forms created
+          <div className="text-base-content/80 text-base">
+            <span className="text-base-content font-semibold">1,567+</span> AI Forms created
+          </div>
         </div>
       </div>
     </div>
