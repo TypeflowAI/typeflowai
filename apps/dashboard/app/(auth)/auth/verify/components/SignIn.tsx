08e8dbd5e6fcdb67109588ffbaae39d9d60fa3a7
ycfra <yayecaceres@gmail.com>
2024-04-20 21:26:00 +0100

    Fix verify email

--- apps/dashboard/app/(auth)/auth/verify/components/SignIn.tsx
+++ apps/dashboard/app/(auth)/auth/verify/components/SignIn.tsx
@@ -1,24 +1,20 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useEffect } from "react";
 
 import { capturePosthogEvent } from "@typeflowai/ui/PostHogClient";
 
 export const SignIn = ({ token }) => {
-  const { data: session, status } = useSession();
-
   useEffect(() => {
     if (token) {
-      signIn("token", { token, callbackUrl: `/` });
+      capturePosthogEvent("EmailConfirmed");
+      signIn("token", {
+        token: token,
+        callbackUrl: `/`,
+      });
     }
   }, [token]);
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      capturePosthogEvent("EmailConfirmed", { email: session.user.email });
-    }
-  }, [status, session]);
 
   return <></>;
 };
