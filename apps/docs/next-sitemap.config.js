9b5a595e0ce41d7d2aa7559b13f34adfd3bf2fd1
ycfra <yayecaceres@gmail.com>
2024-06-08 06:49:23 +0100

    fix: added path docs to sitemap gen

--- apps/docs/next-sitemap.config.js
+++ apps/docs/next-sitemap.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   siteUrl: "https://typeflowai.com",
   generateRobotsTxt: true, // (optional)
+  transform: async (config, path) => {
+    // Agrega '/docs' al inicio de cada ruta generada
+    const newPath = `/docs${path}`;
+    return {
+      loc: `${config.siteUrl}${newPath}`, // `loc` is the path of the URL
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
