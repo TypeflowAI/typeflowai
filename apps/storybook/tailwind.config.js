9b5a57e0906fdfc337a7ea54a64cf82db03d6375
ycfra <yayecaceres@gmail.com>
2024-06-14 11:26:19 +0100

    chore: storybook removed

--- apps/storybook/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import base from "../../packages/config-tailwind/tailwind.config";
-
-export default {
-  ...base,
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "../../packages/ui/**/*.{js,ts,jsx,tsx}"],
-};
