b9a4828c267deacc77d517f465a71e95c0a73978
ycfra <yayecaceres@gmail.com>
2024-06-16 08:15:36 +0100

    fix: ui issues

--- packages/ui/MessageEditor/index.tsx
+++ packages/ui/MessageEditor/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import type { Dispatch, SetStateAction } from "react";
-import { useMemo } from "react";
-import { extractLanguageCodes, isLabelValidForAllLanguages } from "@typeflowai/lib/i18n/utils";
 import { md } from "@typeflowai/lib/markdownIt";
 import type { TI18nString, TWorkflow } from "@typeflowai/types/workflows";
 import { Editor } from "../Editor";
@@ -19,38 +17,14 @@ interface MessageEditorProps {
   setFirstRender?: Dispatch<SetStateAction<boolean>>;
 }
 
-const checkIfValueIsIncomplete = (
-  id: string,
-  isInvalid: boolean,
-  workflowLanguageCodes: string[],
-  value?: TI18nString
-) => {
-  const labelIds = ["subheader"];
-  if (value === undefined) return false;
-  const isDefaultIncomplete = labelIds.includes(id) ? value.default.trim() !== "" : false;
-  return isInvalid && !isLabelValidForAllLanguages(value, workflowLanguageCodes) && isDefaultIncomplete;
-};
-
 export const MessageEditor: React.FC<MessageEditorProps> = ({
-  id,
   value,
-  localWorkflow,
-  isInvalid,
   updateQuestion,
   selectedLanguageCode,
   questionIdx,
   firstRender,
   setFirstRender,
 }) => {
-  const workflowLanguageCodes = useMemo(
-    () => extractLanguageCodes(localWorkflow.languages),
-    [localWorkflow.languages]
-  );
-  const isInComplete = useMemo(
-    () => checkIfValueIsIncomplete(id, isInvalid, workflowLanguageCodes, value),
-    [id, isInvalid, workflowLanguageCodes, value]
-  );
-
   return (
     <div className="relative w-full">
       <Editor
